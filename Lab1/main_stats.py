77e7788bb446697b28c175b79c5502423f8da2fe
Franz Manalili <manalilifranzmiguel@gmail.com>
2024-06-24 17:03:45 +0800

    removed main_stats.py

--- Lab1/main_stats.py
+++
@@ -1,8 +0,0 @@
-#main function for stats.py
-from stats import mean, median, mode
-
-numbers = [3, 2, 5, 6, 5, 2, 2, 7]
-
-print("Mean:", mean(numbers))
-print("Median:", median(numbers))
-print("Mode:", mode(numbers))
